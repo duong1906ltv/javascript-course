8f38d04bb6da7c8ce13061e4b58dae217dc4e3f0
Dinh Duong <duong1906ltv@gmail.com>
2022-07-04 21:54:50 +0700

    Coding Challenge #3

--- PROJECT Bankist App/script.js
+++ PROJECT Bankist App/script.js
@@ -169,7 +169,21 @@ const max = movements.reduce((acc, mov) => {
 }, movements[0]);
 console.log(max);
 
-const calcAverageHumanAge = function (ages) {
+// const calcAverageHumanAge = function (ages) {
+//   const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+//   const adults = humanAges.filter(age => age >= 18);
+//   console.log(humanAges);
+//   console.log(adults);
+
+//   const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+//   return average;
+// };
+
+// const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+// const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+// console.log(avg1, avg2);
+
+const calcAverageHumanAge2 = function (ages) {
   const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
   const adults = humanAges.filter(age => age >= 18);
   console.log(humanAges);
@@ -178,6 +192,12 @@ const calcAverageHumanAge = function (ages) {
   const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
   return average;
 };
+
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
 
 const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
