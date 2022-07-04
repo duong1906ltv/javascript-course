c647c702755560a35297316c1717550d50fec331
Dinh Duong <duong1906ltv@gmail.com>
2022-07-04 21:04:34 +0700

    computing usernames

--- PROJECT Bankist App/script.js
+++ PROJECT Bankist App/script.js
@@ -79,6 +79,18 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+    return username;
+  });
+};
+
+createUsernames(accounts);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
