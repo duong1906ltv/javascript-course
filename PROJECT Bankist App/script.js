2374d74169c41f740ba36843664b9b91c4d115cb
Dinh Duong <duong1906ltv@gmail.com>
2022-07-05 22:42:29 +0700

    The find Method

--- PROJECT Bankist App/script.js
+++ PROJECT Bankist App/script.js
@@ -235,3 +235,12 @@ const totalDepositsUSD = movements
   })
   .reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD);
+
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log(movements);
+console.log(firstWithdrawal);
+
+console.log(accounts);
+
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+console.log(account);
